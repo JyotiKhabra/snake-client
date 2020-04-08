5e498e43ca1d022a5fb927c1bf98f6ac6ea0ed67
vagrant <j_khabra11@hotmail.com>
2020-04-08 22:05:54 +0000

    fix error and export code

---
+++ input.js
@@ -0,0 +1,14 @@
+const handleUserInput = function(data){
+  if (data === '\x03'){
+    process.exit()
+  }
+}  
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+}
+module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,17 +1,6 @@
 const connect = require('./client')
+const setUpInput = require('./input')  
 console.log('Connecting ...');
 connect();
+setUpInput();
 
-const handleUserInput = function(data){
-  if (data === '\x03'){
-    process.exit()
-  }
-} 
-  
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  return stdin;
-}
